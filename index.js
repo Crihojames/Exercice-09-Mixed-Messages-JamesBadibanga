67125b0a560390445be4da55efd3e979608b9814
Crihojames <crihojames@gmail.com>
2023-07-27 12:40:24 +0100

    Fonction pour générer un message aléatoire

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const sujets = ['Le chat', 'Le chien', "L\'oiseau", 'Le lion'];
 const verbes = ['mange', 'dort', 'court', 'saute'];
 const complements = ['un poisson', 'sur le canapé', 'dans le jardin', 'par-dessus la clôture'];
+
+function genererMessage(){
+    const sujet = sujets[Math.floor(Math.random() * sujets.length)];
+  const verbe = verbes[Math.floor(Math.random() * verbes.length)];
+  const complement = complements[Math.floor(Math.random() * complements.length)];
+  const message = `${sujet} ${verbe} ${complement}.`;
+  return message;
+}
