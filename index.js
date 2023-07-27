1d7928657b2d3a6bd9bc81d4873c0d20c9812f04
Crihojames <101941972+Crihojames@users.noreply.github.com>
2023-07-27 11:24:13 +0000

    delete file

--- index.js
+++
@@ -1,3 +0,0 @@
-const sujets = ['le chat', 'le chien' 'L\'oiseau', 'le lion'];
-const verbes = ['image', 'dort', 'court', 'saute'];
-const completemts = ['un poisson', 'sur le canapé', 'dans le jardin', 'par-dessus la clôture'];
