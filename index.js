e88f979dda195c03bdc92170ef1da045f33c1994
Crihojames <crihojames@gmail.com>
2023-07-27 12:36:24 +0100

    premier add

---
+++ index.js
@@ -0,0 +1,3 @@
+const sujets = ['Le chat', 'Le chien', "L\'oiseau", 'Le lion'];
+const verbes = ['mange', 'dort', 'court', 'saute'];
+const complements = ['un poisson', 'sur le canapé', 'dans le jardin', 'par-dessus la clôture'];
