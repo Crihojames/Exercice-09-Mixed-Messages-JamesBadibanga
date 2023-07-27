c76b2f9b0324f587999e41f4590290fdba2fc327
Crihojames <101941972+Crihojames@users.noreply.github.com>
2023-07-27 10:17:26 +0000

    Tableaux de données pour générer des messages aléatoires

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-//push
+const sujets = ['le chat', 'le chien' 'L\'oiseau', 'le lion'];
+const verbes = ['image', 'dort', 'court', 'saute'];
+const completemts = ['un poisson', 'sur le canapé', 'dans le jardin', 'par-dessus la clôture'];
